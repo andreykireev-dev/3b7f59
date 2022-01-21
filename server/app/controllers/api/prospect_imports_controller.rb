e416d75acd70b391edbc0b183e83e66e0878e464
Andrey <andreyk@eqmatch.co>
2022-01-21 23:40:25 +0000

    test framework setup

--- server/app/controllers/api/prospect_imports_controller.rb
+++ server/app/controllers/api/prospect_imports_controller.rb
@@ -1,4 +1,25 @@
 class Api::ProspectImportsController < ApplicationController
   def create
+    respond_to do |format|
+      msg = { 
+        :status => "ok", 
+        :message => "Success!", 
+        :html => "<b>...</b>",  
+        **prospect_import_params
+      }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
   end
+
+  def prospect_import_params
+    params.permit(
+      :file, 
+      :email_index, 
+      :first_name_index, 
+      :last_name_index, 
+      :force, 
+      :has_headers
+    )
+  end
+
 end
--- server/test/controllers/api/prospect_imports_controller_test.rb
+++ server/test/controllers/api/prospect_imports_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class Api::ProspectImportsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get api_prospect_imports_create_url
-    assert_response :success
+  # include Devise::Test::IntegrationHelpers
+
+  test 'create action with uploading a excel should do import' do
+    assert true
+    # position = users(:enterprise_user).company.positions.create! title: "Upload Test"
+    # position.candidates.delete_all
+    # sign_in users(:enterprise_user)
+    # post position_candidates_path(position), params: { candidate: {import_file: fixture_file_upload('files/eqmatch_candidate_import_template_valid_file.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', :binary) }}
+    # assert_response :redirect
+    # position.reload
+    # assert_equal 10, position.candidates.count
+    # assert_match 'The import was succesful', flash[:notice]
+    
+    # will use partial file
+
   end
 end
