d37ba2a314739834302245db7768fd032252eaf4
Andrey <andreyk@eqmatch.co>
2022-01-31 17:13:00 +0000

    cleanup per comments

--- server/config/environments/test.rb
+++ server/config/environments/test.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  # config.cache_store = :null_store
   config.cache_store = :memory_store
 
 
